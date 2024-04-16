c1eabab27d6131cca402eaeb40cd1314ef34e62c
Valritehachi <valritehachi@gmail.com>
2024-04-16 12:04:57 -0700

    edits

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+""" test amenity function """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """class amenity function """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ innitialization test"""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """ test name innitialization """
         new = self.value()
         self.assertEqual(type(new.name), str)
