e42dc78289ae8cce597af5fd3aa7386377eca2cc
Valritehachi <valritehachi@gmail.com>
2024-05-19 03:52:55 -0700

    edits

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -12,21 +12,25 @@ def hello_hbnb():
     """ a function that Prints in the Web """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hbnb():
     """ a function that Prints in the Web """
     return 'HBNB'
+
 
 @app.route('/c/<text>')
 def c_is_fun(text):
     """ a function that Prints in the Web """
     return 'C {}'.format(text.replace('_', ' '))
 
+
 @app.route('/python')
 @app.route('/python/<text>')
 def python_is_cool(text='is cool'):
     """ a function that Prints in the Web """
     return 'Python {}'.format(text.replace('_', ' '))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
